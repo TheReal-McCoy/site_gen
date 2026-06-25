c85fa7aeb39555df95418485a24bc6f416701908
TheReal-McCoy <mccoyj5998@gmail.com>
2026-06-25 18:05:27 -0400

    3.1 done

---
+++ src/test_inline_markdown.py
@@ -0,0 +1,22 @@
+import unittest
+from inline_markdown import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+class TestInlineMarkdown(unittest.TestCase):
+    def test_delim_bold(self):
+        node = TextNode("This is text with a **bolded** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual([TextNode("This is text with a ", TextType.TEXT), TextNode("bolded", TextType.BOLD), TextNode(" word", TextType.TEXT)], new_nodes)
+
+    def test_delim_bold_double(self):
+        node = TextNode("This is text with a **bolded** word and **another**", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word and ", TextType.TEXT),
+                TextNode("another", TextType.BOLD),
+            ],
+            new_nodes,
+        )
